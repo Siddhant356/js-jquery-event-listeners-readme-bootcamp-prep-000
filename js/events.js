a55e5e49fb366a0aefc65c0e18366db26b2fdf69
Siddhant Shukla <siddhantshukla356@gmail.com>
2019-05-02 08:34:21 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,14 @@
-//define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
 
+function frameIt(){
+  $('img').on('load', function(){
+    
+  })
+}
 $(document).ready(function(){
 
 // call functions here
