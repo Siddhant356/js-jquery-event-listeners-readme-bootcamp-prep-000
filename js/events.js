48e02c947f64b278be0e10d36f9566f168992406
Siddhant Shukla <siddhantshukla356@gmail.com>
2019-05-02 08:45:36 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -17,7 +17,7 @@ function pressIt(){
 }
 function submitIt(){
   $('form').on('submit', function(){
-    alert('')
+    alert('Your form is going to be submitted now.')
   })
 }
 $(document).ready(function(){
